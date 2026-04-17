68e811dbddeb1a41ef72733a475901e46c01eabf
JonathasMenezes <jonathasbstera11@gmail.com>
2026-04-17 20:08:35 -0300

    Add files via upload

---
+++ src/components/components/Header.js
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <header className="header">
+      <h1>JONATHAS MENEZES</h1>
+      <nav>
+        <a href="#sobre">Sobre</a>
+        <a href="#projetos">Projetos</a>
+        <a href="#contato">Contato</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
