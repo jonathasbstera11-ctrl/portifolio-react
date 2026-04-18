34fe08b6f93c0382bd7fea28b56d2b1e5874038a
JonathasMenezes <jonathasbstera11@gmail.com>
2026-04-17 21:35:14 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Header from "./components/Header";
+import Sobre from "./components/Sobre";
+import Projetos from "./components/Projetos";
+import Contato from "./components/Contato";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Sobre />
+      <Projetos />
+      <Contato />
+    </div>
+  );
+}
+
+export default App;
